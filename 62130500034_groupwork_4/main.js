56c24fb3e51cfba887c1250fe997872da43797ae
UntillNIGHT <lengleng9090@gmail.com>
2021-02-18 12:46:54 +0700

    [ADD]File Homework

---
+++ 62130500034_groupwork_4/main.js
@@ -0,0 +1,41 @@
+const app = {
+    data() {
+        return {
+            picture: [{title: 'Geysers Valley',country: 'Russia',url: './images/Geysers Valley.jpg',like:false},
+                    {title: 'Khumbu Valley',country: 'Nepal', url: './images/Khumbu Valley.jpg',like:false},
+                    {title: 'Waipi’o Valley',country: 'Hawaii', url: './images/Waipi’o Valley.jpg',like:false},
+                    {title: 'Doi Inthanon',country: 'Thailand',url:'./images/Doi Inthanon.jpg',like:false}
+                ],
+        filterResult:null,
+        useSearch : false,
+        hasView : false,
+        viewImg:'',
+        inputSearch:''
+        }
+    },
+    methods: {
+        toggleLike(index){
+            this.picture[index].like = !this.picture[index].like
+        },
+        toggleSearch(){
+            this.filterResult = null;
+            this.useSearch = !this.useSearch
+        },
+        toggleView(index){
+            this.viewImg = this.picture[index].url;
+            this.hasView = true;
+        }
+    },
+    
+    computed:{
+        filterSearch(){
+            this.filterResult = this.picture.filter(pic => pic.title.toLowerCase().search(this.inputSearch.toLowerCase()) > -1);
+            return this.inputSearch;
+    },
+     choosePicture(){
+         return this.filterResult ?  this.filterResult : this.picture;
+     }
+    }
+
+}
+Vue.createApp(app).mount('#app')
